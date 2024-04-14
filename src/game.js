ce92b8baab48240933c966c872bad131341e4c0d
Anna DI PLACIDO <anna.di-placido@etu.unice.fr>
2024-04-14 17:07:45 +0200

    server modif

--- src/game.js
+++ src/game.js
@@ -3,10 +3,11 @@ import { Engine } from "@babylonjs/core";
 import Game from "./gamesTypes/bowlRace";
 const Colyseus = require('colyseus.js');
 
+
 const client = new Colyseus.Client('ws://localhost:2567');
 
-client.joinOrCreate("my_room", {/* options */ }).then(room => {
-    console.log("joined successfully"/*, room*/);
+client.joinOrCreate("my_room", { name: "Race" }).then(room => {
+    console.log("joined successfully", room);
 }).catch(e => {
     console.error("join error", e);
 });
